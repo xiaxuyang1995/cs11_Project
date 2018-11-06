1741158f1267ffc456a556731390a30bf520f099
Andrew Yan <srdrewyan@gmail.com>
2018-11-06 15:44:18 -0500

    Project beginning

--- projectalpha.java
+++ projectalpha.java
@@ -1,3 +1,14 @@
-public class projectalpha{
-  public static void main(String[] args){}
+public class Projectalpha{
+  public static void main(String[] args){
+    welcome();
+  }
+
+
+  public static void welcome(){
+    System.out.println("Hello there!!");
+    System.out.printf("Welcome to the Food Heaven simulator!%nAt here, we'll try to satisfy all your hunger!!%n");
+    System.out.println("Please insert your name");
+    String username = TextIO.getln();
+    System.out.printf("%s ? Nice name",username);
+  }
 }
