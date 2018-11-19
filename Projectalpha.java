a50a7dce17507be741024d4bc5013a4117279ff3
Andrew Yan <srdrewyan@gmail.com>
2018-11-19 13:27:35 -0500

    1

--- Projectalpha.java
+++ Projectalpha.java
@@ -1,4 +1,5 @@
 public class Projectalpha{
+
   public static void main(String[] args){
     welcome();
   }
@@ -8,7 +9,7 @@ public class Projectalpha{
     System.out.println("Hello there!!");
     System.out.printf("Welcome to the Food Heaven simulator!%nAt here, we'll try to satisfy all your hunger!%n");
     System.out.println("Please insert your name");
-    String username = TextIO.getln();
+    public static final String username = TextIO.getln();
     System.out.printf("%s ? Nice name",username);
   }
 
