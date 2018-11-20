2e3e9d36671341f00f08a665e8c28b2ed17d98e1
ella1120 <mao.dan@hotmail.com>
2018-11-19 23:19:04 -0500

    Nov. 19 uploaded

---
+++ Ella.java
@@ -0,0 +1,56 @@
+// 别管这个程序这只是我自己测试我的部分用的
+import java.util.Arrays;
+
+public class Ella {
+  public static void main(String[] args){
+
+    System.out.println("Hello there!!");
+    System.out.printf("Welcome to the Food Heaven simulator!%nAt here, we'll try to satisfy all your hunger!%n");
+    System.out.println("Please insert your name");
+    String username = TextIO.getln();
+    System.out.printf("%s? Nice name!%n",username);
+
+    TextIO.readFile("dataA.txt");
+    double[][] data = new double[23][13];
+      for (int i=0;i<23;i++) {
+        for(int j=0; j<13; j++){
+          data[i][j]=TextIO.getDouble();
+        }
+      }
+
+
+
+    TextIO.readFile("item.txt");
+    String item = "";
+    while (!TextIO.eof()){
+      item = TextIO.getln();
+    }
+
+    String itemA[] = item.split(",");
+
+    //System.out.println(Arrays.toString(itemA));
+
+
+    TextIO.readFile("nutri.txt");
+    String nutri = "";
+    while (!TextIO.eof()){
+      nutri = TextIO.getln();
+    }
+    String nutriA[] = nutri.split(",");
+
+    //System.out.println(Arrays.toString(nutriA));
+
+
+    System.out.printf("Hey %s, here is the Menu!%n",username);
+
+    System.out.println("                                       MENU");
+    System.out.printf("%30s                        %s%n"," ","Price");
+    for(int i=0;i<itemA.length; i++){
+      if(i!=1 && i!=2 && i!=4 && i!=5 && i!=7 && i!=8){
+        System.out.printf("%30s........................%.2f$%n",itemA[i],data[i][12]);
+      }
+
+    }
+
+  }
+}
--- Projectalpha.java
+++ Projectalpha.java
@@ -1,22 +1,9 @@
 public class Projectalpha{
   public static void main(String[] args){
-    if (args.length==0) {
-      // if not when print an error message and return to the shell
-      System.out.println("You must supply a file name:  java Averages FILENAME.txt");
-      return;
-    }
 
-    TextIO.readFile(args[0]);
-
-    double[][] items = new double[23][13];
-        for (int i=0;i<23;i++) {
-            for(int j=0; j<13; j++){
-              items[i][j]=TextIO.getDouble();
-            }
-        }
-
-    }
     welcome();
+    menuData();
+    printMenu();
 
   }
 
@@ -25,62 +12,69 @@ public class Projectalpha{
     System.out.printf("Welcome to the Food Heaven simulator!%nAt here, we'll try to satisfy all your hunger!%n");
     System.out.println("Please insert your name");
     String username = TextIO.getln();
-    System.out.printf("%s ? Nice name",username);
+    System.out.printf("%s? Nice name!%n",username);
   }
 
 
-  public static void menu(){
-    //这一段想用scanner的但是还没写好所以先放着吧：））
-    /* String item = "Hamburger w/Onion,with mustard & ketchup instead of spread,ProteinÂ® Style (Bun replaced with Lettuce),Cheeseburger w/Onion,with mustard & ketchup instead of spread,ProteinÂ® Style (Bun replaced with Lettuce),Double-Double w/Onion with mustard & ketchup instead of spread,ProteinÂ® Style (Bun replaced with Lettuce),French Fries,Chocolate Shake,Vanilla Shake,Strawberry Shake,Coca-Cola Classic,Diet Coke,Seven Up,Dr Pepper,Root Beer,Lemonade,Minute Maid Light Lemonade,Iced Tea,Coffee,Milk";
-    String item[] = item.split(",");
+  public static void menuData(){
+    TextIO.readFile("dataA.txt");
+    double[][] data = new double[23][13];
+      for (int i=0;i<23;i++) {
+        for(int j=0; j<13; j++){
+          data[i][j]=TextIO.getDouble();
+        }
+      }
 
-    String nutri = "Calories,Total Fat % Daily Values*,Cholesterol % Daily Values*,Sodium % Daily Values*,Carbohydrates % Daily Values*,Dietary Fiber % Daily Values*,Sugars (g),Protein (g),% Vitamin A,% Vitamin C,% Calcium,% Iron";
-    String nutri[] = nutri.split(",");
+      TextIO.readFile("item.txt");
+      String item = "";
+      while (!TextIO.eof()){
+        item = TextIO.getln();
+      }
 
-    TextIO.readFile(dataA.txt);
-    String data[][] = new String[23][13];
-    data.split(" ");
+      String itemA[] = item.split(",");
+
+      //System.out.println(Arrays.toString(itemA));
 
 
-    public static final String FILENAME="dataA.xlsx";
-    public static final int ROWS = 23;
+      TextIO.readFile("nutri.txt");
+      String nutri = "";
+      while (!TextIO.eof()){
+        nutri = TextIO.getln();
+      }
+      String nutriA[] = nutri.split(",");
 
-    public static String[] date = new String[ROWS];
-    public static String[] time = new String[ROWS];
-    public static int[] transaction = new int[ROWS];
-    public static String[] item = new String[ROWS];
+      //System.out.println(Arrays.toString(nutriA));
+  }
 
-    public static void readStaticData(){
-      //Date,Time,Transaction,Item
-      try {
-          File file = new File(FILENAME);
 
-          Scanner fileScanner = new Scanner(file);//use a scanner to read from the file
+    /**
+    这个就是之后调用data array里面数据的时候可以用的格式
+        switch (price){
+          case "Hamburger w/Onion": price=data[0][12]; break;
+          case "Hamburger with mustard & ketchup instead of spread": days=28; break;
+          case "mar": days=31; break;
+          case "apr": days=30; break;
+          case "may": days=31; break;
+          case "jun": days=30; break;
+          case "jul": days=31; break;
+          case "aug": days=31; break;
+          case "sep": days=30; break;
+          case "oct": days=31; break;
+          case "nov": days=30; break;
+          case "dec": days=31; break;
+          default: days=0;
+    */
 
-          // now read the entire file and print it with line numbers:
-          int row=0;
-          while (fileScanner.hasNextLine()) {
-              String line = fileScanner.nextLine();
-              // create a new Scanner just for this row!
-              Scanner lineScanner = new Scanner(line);
-              lineScanner.useDelimiter(",");
-              // we read each of the four columns in this row
-              date[row]=lineScanner.next();
-              time[row] = lineScanner.next();
-              transaction[row] = lineScanner.nextInt();
-              item[row] = lineScanner.next();
-              row++;
-          }
-          fileScanner.close();
-
-      } catch (Exception ex) {
-          ex.printStackTrace();
+  public static void printMenu(){
+    System.out.printf("Hey %s, here is the Menu for today!%n",username);
+    System.out.println("                                       MENU");
+    System.out.printf("%30s                        %s%n"," ","Price");
+    for(int i=0;i<itemA.length; i++){
+      if(i!=1 && i!=2 && i!=4 && i!=5 && i!=7 && i!=8){
+        System.out.printf("%30s........................%.2f$%n",itemA[i],data[i][12]);
       }
     }
   }
-  */
-
-
 
   public static void ordering(){
     //in this method we let the customers choose their meal
@@ -88,13 +82,14 @@ public class Projectalpha{
 
   public static void calories(){
     //in this method we calculate the calories based on the customer's choice
+  }
 
   public static void check(){
     //in this method we calculate the bill
   }
 
   public static void thanks(){
-    System.out.println("Thank you very much for visiting  our restaurant. We will be happy to return to serve you with our best service. We hope to see you again soon!")
+    System.out.println("Thank you very much for visiting  our restaurant. We will be happy to return to serve you with our best service. We hope to see you again soon!");
   }
 
 }
