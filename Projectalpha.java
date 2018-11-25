1712cba43123394087a5bdddbf385892ba2aa1b2
Kexin Zhang <kexin.s.zhang@gmail.com>
2018-11-24 19:20:15 -0500

    zkx

--- Projectalpha.java
+++ Projectalpha.java
@@ -8,6 +8,9 @@ public class Projectalpha{
     menuData();
     printMenu();
     order();
+    calories();
+    check();
+    thanks();
   }
 
   public static void welcome(){
@@ -85,55 +88,50 @@ public class Projectalpha{
   public static int[][] order(){
     //in this method we let the customers choose their meal
     Scanner order= new Scanner(System.in);
-    int [][] orderarray = new int[22][2];
+    int [][] orderarray = new int[23][2];
     System.out.printf("What would you like ? %nPlease enter the item's corresponding number once at a time%n");
     int numberofitem=0;
     int itemnumber=0;
-      while(order.hasNextInt()&& numberofitem<=22){
+      while(order.hasNextInt()&& numberofitem<23){
         itemnumber=Integer.valueOf(order.nextLine());
         orderarray[numberofitem][0]= itemnumber;
 
-          if(itemnumber==0){
+          if(itemnumber==1){
           System.out.println("Would you like your burger to be Animal Style? ");
           String answer = order.nextLine();
             if(answer.equals("yes")){itemnumber+=2;}
             else{itemnumber+=0;}
-
           }
 
-
-          else if(itemnumber==1){
+          else if(itemnumber==2){
           System.out.println("Would you like your burger to be Animal Style? ");
           String answer2 = order.nextLine();
             if(answer2.equals("yes")){itemnumber+=4;}
             else{itemnumber+=2;}
-
           }
 
-
-          else if(itemnumber==2){
+          else if(itemnumber==3){
           System.out.println("Would you like your burger to be Animal Style? ");
           String answer3 = order.nextLine();
             if(answer3.equals("yes")){itemnumber+=6;}
             else{itemnumber+=4;}
-
           }
 
-          else {itemnumber+=6;}
+          else {
+            itemnumber+=6;
+          }
 
         orderarray[numberofitem][0]= itemnumber;
 
-        System.out.printf("How many of #%d would you want? ", menuData.itemA[itemnumber]);
+        System.out.printf("How many of #%d would you want? ", menuData.itemA[itemnumber-1]);
         int itemquantity= order.nextInt();
         orderarray[numberofitem][1]= itemquantity;
-        System.out.printf("%d of #%d%n",itemquantity,menuData.itemA[itemnumber]);
+        System.out.printf("%d of #%d%n",itemquantity,menuData.itemA[itemnumber-1]);
         System.out.printf("what other item would you like?%n(To finish your order, enter 'finish')%n");
         numberofitem++;
         order.nextLine();
 
       }
-
-
 
     int[][] customer= new int [numberofitem][2];
     for(int i=0;i<customer.length;i++){
@@ -156,10 +154,42 @@ public class Projectalpha{
 
   public static void calories(){
     //in this method we calculate the calories based on the customer's choice
+    double totalCalories = 0;
+    for (int i=0; i<orderarray.length; i++){
+      double calories = data[(orderarray[i][0])-1][0]*orderarray[i][1];
+      totalCalories = totalCalories+calories;
+      System.out.printf("The calories for %s is %.2f ", menuData.itemA[itemnumber-1],calories);
+    }
+    System.out.println("The total amount of calorie is"+totalCalories);
+    System.out.println("Are you satisfied with the calories? (Enter yes or no)");
+    Scanner order= new Scanner(System.in);
+    String answer = order.nextLine();
+    if (answer.equals("yes")){
+        System.out.println("Preceeding to checkout");
+    }
+    else{
+      order();
+    }
   }
 
   public static void check(){
     //in this method we calculate the bill
+    double totalPrice = 0;
+    for (int i=0; i<orderarray.length; i++){
+      double price = data[(orderarray[i][0])-1][12]*orderarray[i][1];
+      totalPrice = totalPrice+price;
+    }
+    System.out.println("The total price is "+totalPrice);
+    System.out.println("Do you want to split the bill?");
+    Scanner order= new Scanner(System.in);
+    String answer = order.nextLine();
+    if(answer.equals("yes")){
+      System.out.println("How many people?");
+      int people = TextIO.getlnInt();
+      System.out.println("Each should pay "+(totalPrice/people));
+    }else{
+
+    }
   }
 
   public static void thanks(){
